3b711b47cf709c9238d4bc657cfe431c2975d7a6
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2022-05-25 10:39:40 -0500

    upd

---
+++ 0_hello/src/getting_started.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+
+
+
+/* This may look like nonsense, but really is -*- mode: C -*- */
+#include <stdlib.h>
+
+int main (void) {
+  // Declarations
+  double A[5] = {
+    [0] = 9.0,
+    [1] = 2.9,
+    [4] = 3.E+25,
+    [3] = .00007
+  };
+
+  // Doing some work
+  for (size_t i = 0; i < 5; ++i) {
+    printf("Element %zu is %g, \tits square is %g\n",
+           i,
+           A[i],
+           A[i] * A[i]);
+  }
+
+  return EXIT_SUCCESS;
+}
