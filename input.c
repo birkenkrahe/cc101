786ee8a7ee33f410340d537ed08d8781e926a065
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2022-06-03 18:48:34 -0500

    update

---
+++ input.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+
+
+
+int main() {
+// declare integer
+int i;
+// ask for input
+printf("Enter an integer number: ");
+// get input
+scanf("%d", &i);
+// print input
+printf("You entered: %d\n", i);
+return 0;
+}
