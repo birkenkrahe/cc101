80f3a03f56ed8cd2df883b370e0d5aa8c24843d2
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2022-06-16 11:08:11 -0500

    updates

--- 6_loops/src/checking.c
+++ 6_loops/src/checking.c
@@ -13,11 +13,37 @@ int main(void)
   int cmd; // user choice 0...4
   float balance = 0.0f, credit, debit;
 
+  // User instructions
   printf("*** ACME checkbook-balancing program ***\n");
-  printf("
+  printf("Commands: 0=clear, 1=credit, 2=debit, ");
+  printf("3=balance, 4=exit\n\n");
 
-
-
-
-         return 0;
-         }
+  for(;;) {  // do this forever until exit=4
+    printf("Enter command: ");
+    scanf("%d", &cmd);
+    switch (cmd) { 
+    case 0:            // clear balance
+      balance = 0.0f;
+      break;
+    case 1:            // credit amount
+      printf("Enter amount of credit: ");
+      scanf("%f", &credit);
+      balance += credit;
+      break;
+    case 2:            // debit amount
+      printf("Enter amount of debit: ");
+      scanf("%f", &debit);
+      balance -= debit;
+      break;
+    case 3:          // print balance
+      printf("Current balance: $%.2f\n", balance);
+      break;
+    case 4:
+      return 0;
+    default:
+      printf("Commands: 0=clear, 1=credit, 2=debit, ");
+      printf("3=balance, 4=exit\n\n");
+      break;
+    }
+  }
+}
