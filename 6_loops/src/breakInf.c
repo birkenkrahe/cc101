8ceddde62d64b6f3bf238f045337eabf3a6cc0e0
Marcus Birkenkrahe <birkenkrahe@lyon.edu>
2022-06-16 14:55:29 -0500

    update

--- 6_loops/src/breakInf.c
+++ 6_loops/src/breakInf.c
@@ -1,24 +1,12 @@
 
-
-/* - Here's some code: what does it do? What would happen without the */
-/*   ~break~ statement? */
-
-/*   #+name: pgm:break */
-
-/* [[file:../README.org::pgm:break][pgm:break]] */
 #include <stdio.h>
-
-
-
-
-
 
 int main() {
 int n;
 for (;;) {
   scanf("%d", &n);
-  if (n == 0)
-    printf("loop: n is %d\n", n);
+  if (n == 0);
+  printf("loop: n is %d\n", n);
  }
 printf("n is %d\n", n);
 return 0;
